05649abca3341a2c2bd1508f33d7f91f777cf7ba
Sven Heinicke <sven@genomics.princeton.edu>
2014-09-24 15:23:21 +0000

    added participant_types table

--- sql/Participant.sql
+++ sql/Participant.sql
@@ -1,17 +1,26 @@
+-- table described in the comment of the docs, not in the docs itself.
+CREATE TABLE IF NOT EXISTS participant_types(
+       participant_type_id	  BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
+       participant_type_name	  VARCHAR(255)UNIQUE,
+       participant_type_status	  ENUM('active','inactive'),
+       participant_type_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO participant_types(participant_type_name)VALUES('Gene');
+
 CREATE TABLE IF NOT EXISTS participants(
        participant_id	     BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
        participant_value     BIGINT(10)NOT NULL, -- phony foreign key
-       participant_type_id   BIGINT(10)NOT NULL,
+       participant_type_id   BIGINT(10)NULL, -- null meant unknow participant
        participant_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
        participant_status    ENUM('active','inactive')NOT NULL DEFAULT 'active',
+       FOREIGN KEY(participant_type_id)REFERENCES participant_types(participant_type_id),
        UNIQUE KEY(participant_value,participant_type_id)
-              
 );
 
-       
 CREATE TABLE IF NOT EXISTS participant_roles(
        participant_role_id        BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
-       participant_role_name	  VARCHAR(255)unique,
+       participant_role_name	  VARCHAR(255)UNIQUE,
        participant_role_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
        participant_role_status	  ENUM('active','inactive')NOT NULL DEFAULT 'active'
 );
--- sql/Unknown_participant.sql
+++ sql/Unknown_participant.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS unknown_participants(
        unknown_participant_id	      BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
        unknown_participant_identifier VARCHAR(255)NOT NULL,
-       participant_type_id	      VARCHAR(255)NOT NULL,
+       participant_type_id	      BIGINT(10)NOT NULL,
        organism_id		      BIGINT(10)NULL,
        publication_id		      BIGINT(10)NOT NULL,
        unknown_participant_addeddate  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
        unknown_participant_status     ENUM('active','inactive')NOT NULL DEFAULT 'active',
        FOREIGN KEY(publication_id)REFERENCES publications(publication_id),
+       FOREIGN KEY(participant_type_id)REFERENCES participant_types(participant_type_id),
        UNIQUE KEY(unknown_participant_identifier,participant_type_id,organism_id,publication_id)
-)
+);
