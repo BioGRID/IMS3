9898573231b4829b153ba30d315cb1e4aa0ac756
Sven Heinicke <sven@genomics.princeton.edu>
2014-12-03 19:18:24 +0000

    turned on foreign keys

--- sql/Ontology_relationship.sql
+++ sql/Ontology_relationship.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS  ontology_relationships(
        ontology_relationship_type      VARCHAR(255)NOT NULL,
        ontology_relationship_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
        ontology_relationship_status    ENUM('active','inactive')NOT NULL DEFAULT 'active'
---       ,FOREIGN KEY(ontology_term_id)REFERENCES ontologies(ontology_id)
---       ,FOREIGN KEY(ontology_parent_id)REFERENCES ontologies(ontology_id)
-)
+       ,FOREIGN KEY(ontology_term_id)REFERENCES ontology_terms(ontology_term_id)
+       ,FOREIGN KEY(ontology_parent_id)REFERENCES ontology_terms(ontology_term_id)
+);
+
+-- INSERT INTO ontology_relationships(ontology_relationship_id,ontology_term_id,
