13b3e8fdf2eba52befce0bbbfe56c709955a4385
Sven Heinicke <sven@genomics.princeton.edu>
2014-12-08 11:18:01 +0000

    added some lesser tables

--- sql/Interaction_ontology.sql
+++ sql/Interaction_ontology.sql
@@ -35,3 +35,14 @@ CREATE TABLE IF NOT EXISTS interaction_ontologies(
        ,FOREIGN KEY(user_id)REFERENCES users(user_id)
        ,FOREIGN KEY(interaction_ontology_type_id)REFERENCES interaction_ontology_types(interaction_ontology_type_id)
 );
+
+CREATE TABLE IF NOT EXISTS interaction_ontologies_notes(
+       interaction_ontology_note_id        BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
+       interaction_ontology_id             BIGINT(10)NOT NULL,
+       interaction_ontology_note_text      TEXT NOT NULL,
+       user_id                             BIGINT(10)NOT NULL,
+       interaction_ontology_note_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       interaction_ontology_note_status    ENUM('active','inactive')NOT NULL DEFAULT 'active'
+       ,FOREIGN KEY(interaction_ontology_id)REFERENCES interaction_ontologies(interaction_ontology_id)
+       ,FOREIGN KEY(user_id)REFERENCES users(user_id)
+);
--- sql/Interaction_participant.sql
+++ sql/Interaction_participant.sql
@@ -4,8 +4,19 @@ CREATE TABLE IF NOT EXISTS interaction_participants(
        participant_id			 BIGINT(10)NOT NULL,
        participant_role_id		 BIGINT(10)NOT NULL,
        interaction_participant_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-       interaction_participant_status	 ENUM('active','inactive')NOT NULL DEFAULT 'active',
-       FOREIGN KEY(interaction_id)REFERENCES interactions(interaction_id)ON DELETE CASCADE,
-       FOREIGN KEY(participant_id)REFERENCES participants(participant_id),
-       FOREIGN KEY(participant_role_id)REFERENCES participant_roles(participant_role_id)
-)
+       interaction_participant_status	 ENUM('active','inactive')NOT NULL DEFAULT 'active'
+       ,FOREIGN KEY(interaction_id)REFERENCES interactions(interaction_id)ON DELETE CASCADE
+       ,FOREIGN KEY(participant_id)REFERENCES participants(participant_id)
+       ,FOREIGN KEY(participant_role_id)REFERENCES participant_roles(participant_role_id)
+);
+
+CREATE TABLE IF NOT EXISTS interaction_participant_notes(
+       interaction_participant_note_id        BIGINT(10)PRIMARY KEY AUTO_INCREMENT,
+       interaction_participant_id	      BIGINT(10)NOT NULL,
+       interaction_participant_note_text      TEXT NOT NULL,
+       user_id                                BIGINT(10)NOT NULL,
+       interaction_participant_note_addeddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       interaction_participant_notes_status   ENUM('active','inactive')NOT NULL DEFAULT 'active'
+       ,FOREIGN KEY(interaction_participant_id)REFERENCES interaction_participants(interaction_participant_id)
+       ,FOREIGN KEY(user_id)REFERENCES users(user_id)
+);
