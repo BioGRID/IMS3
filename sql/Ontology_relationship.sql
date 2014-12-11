12324fb43ba52d0e10767ddb154136b4f2a0f4f0
Sven Heinicke <sven@genomics.princeton.edu>
2014-12-11 19:21:16 +0000

    added step type, a node above all leaves

--- sql/Ontology_relationship.sql
+++ sql/Ontology_relationship.sql
@@ -9,11 +9,13 @@ CREATE TABLE IF NOT EXISTS  ontology_relationships(
        ,FOREIGN KEY(ontology_parent_id)REFERENCES ontology_terms(ontology_term_id)
 );
 
+
+
 INSERT INTO ontology_relationships
 (ontology_relationship_id,ontology_term_id,ontology_parent_id,ontology_relationship_type)VALUES
 ( 1, 1,NULL,'root'), -- root
 ( 2, 2,   1,'head'), -- Biochemical
-( 3, 3,   2,'head'), -- In vivo
+( 3, 3,   2,'stem'), -- In vivo
 ( 4, 4,   3,'leaf'),
 ( 5, 5,   3,'leaf'),
 ( 6, 6,   3,'leaf'),
@@ -25,14 +27,14 @@ INSERT INTO ontology_relationships
 (12,12,   3,'leaf'),
 (13,13,   3,'leaf'),
 (14,14,   3,'leaf'),
-(15,15,   2,'head'), -- In vitro
+(15,15,   2,'stem'), -- In vitro
 (16,16,  15,'leaf'),
 (17,17,  15,'leaf'),
 (18,18,  15,'leaf'),
 (19,19,  15,'leaf'),
 (20,20,  15,'leaf'),
 (21,21,  15,'leaf'),
-(22,22,   1,'head'), -- Genetic
+(22,22,   1,'stem'), -- Genetic
 (23,23,  22,'leaf'),
 (24,24,  22,'leaf'),
 (25,25,  22,'leaf'),
