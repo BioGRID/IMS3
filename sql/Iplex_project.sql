3968adaa2a36b9901a0f2ebb331402c6078058d0
Sven Heinicke <sven@genomics.princeton.edu>
2015-01-13 18:13:08 +0000

    now loading participant_tags

--- sql/Iplex_project.sql
+++
@@ -1,9 +0,0 @@
--- Some day I'll figure out what an iplex is
-CREATE TABLE IF NOT EXISTS iplex_projects(
-       iplex_project_id			BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
-       iplex_project_name		VARCHAR(255) UNIQUE NOT NULL,
-       iplex_project_fullname		VARCHAR(255) NULL,
-       iplex_project_description	TEXT NULL,
-       iplex_project_addeddate		TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-       iplex_project_status		ENUM('active','inactive') NOT NULL
-);
