9e9e57624790e5a05a6b9e1d4f8530bdfeb28256
Sven Heinicke <sven@genomics.princeton.edu>
2015-03-27 17:13:16 +0000

    Revert "now displays interaction_ontology_qualifiers better"
    This reverts commit 46eab2d9068ddbbfac6428f0e064cf423b86de7f.

--- www/Interaction_ontology_qualifier.js
+++ www/Interaction_ontology_qualifier.js
@@ -2,10 +2,7 @@ IMS.Interaction_ontology_qualifier=function(data){
   this.data=data;
 }
 
-// The PHP query for Interaction_ontology_qualifier joins the
-// Ontology_term row, so when printing we want to use the same html()
-// function.
-IMS.Interaction_ontology_qualifier.prototype=new IMS.Ontology_term();
+IMS.Interaction_ontology_qualifier.prototype=new IMS._table();
 IMS.Interaction_ontology_qualifier.prototype._const={
   'table':'interaction_ontologies_qualifiers',
   'primary_col':'interaction_ontology_qualifier_id',
