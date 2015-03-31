8d25d96ff699ed2ad0ac53c09920c9dbe3e8c000
Sven Heinicke <sven@genomics.princeton.edu>
2015-03-31 18:44:32 +0000

    added ontorolgy_term id to tooltip

--- www/Ontology_term.js
+++ www/Ontology_term.js
@@ -11,5 +11,10 @@ IMS.Ontology_term.prototype._const={
 IMS.Ontology_term.prototype.html=function(){
   var name=this.data.ontology_term_name;
   var desc=this.data.ontology_term_desc;
+  if(desc){
+    desc=this.data.ontology_term_official_id + ': ' + desc;
+  }else{
+    desc=this.data.ontology_term_official_id;
+  }
   return '<span data-toggle="tooltip" title="' + desc + '">' + name + '</span>';
 }
